63ecacd33ffd5339307476daa5ba404180139a5b
powerlanguage <josh.wardle@gmail.com>
2016-04-29 16:16:40 -0700

    Start on adding audio.

---
+++ Assets/Scripts/MusicControlScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicControlScript : MonoBehaviour {
+
+	void Awake () {
+		DontDestroyOnLoad (this.gameObject);
+	}
+
+}
---
+++ Assets/Scripts/StartScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class StartScript : MonoBehaviour {
+
+	void OnMouseUp(){
+		SceneManager.LoadScene ("TestScene");
+	}
+}
